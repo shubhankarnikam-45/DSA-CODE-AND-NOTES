e3526a57933ba90cbde40566e5a55aaacb9ced61
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-08-05 09:51:17 +0530

    adding the code of two sum which using TWO POINTER AND GREDDY APPROACH.

---
+++ ARRAY/twoSumUsingTwoPointer.java
@@ -0,0 +1,30 @@
+//here we use the GREEDY APPROACH.
+ boolean hasArrayTwoCandidates(int arr[], int n, int target) {
+        //first of all make array sorted. to apply greedy approach.
+         Arrays.sort(arr);
+         // make 'left=0'
+        int left=0;
+        //make 'right=n-1'
+        int right=n-1;
+        
+        while(left<right)
+        {
+            int sum=arr[left]+arr[right];
+            if(sum==target)
+            {  
+                return true; 
+            }
+            else if(sum<target)
+            {
+                left++;
+            }
+            else        //here (sum>target)
+            {
+                right--;
+            }
+        }
+        
+        return false;
+    }
+
+//must see the notes.
