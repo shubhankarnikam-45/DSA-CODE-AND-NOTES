7bd96bd4911c12d96ed0b1b25f2fbf7e4968e086
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-08-08 14:26:45 +0530

    adding theory of why swapping reqired

--- RECURSION/FOR CODES/Recursive Multiplication/recusiveMultiplication2.java
+++ RECURSION/FOR CODES/Recursive Multiplication/recusiveMultiplication2.java
@@ -13,3 +13,18 @@ public static int multiply(int n, int m) {
     else
       return 0;
   }
+
+
+  //here we do swping because of 
+
+  Swapping the values of `n` and `m` when `n` is less than `m` in the given recursive method (`multiply(n, m)`) doesn't necessarily provide a benefit in terms of computation or efficiency. Instead, it's a technique used to ensure that the larger value is always assigned to `n` and the smaller value to `m` before the recursive calculations are performed. This adjustment is made to maintain consistency and simplify the recursive logic.
+
+In the context of the provided recursive multiplication method, swapping `n` and `m` when `n` is less than `m` achieves the following:
+
+1. **Consistency**: Swapping ensures that regardless of the order in which `n` and `m` are provided as arguments, the algorithm consistently treats the larger value as `n`. This makes it easier to reason about how the recursion is working and simplifies the base and recursive cases.
+
+2. **Simplified Recursive Logic**: By ensuring that `n` is always greater than or equal to `m`, the recursive logic doesn't need to account for cases where `m` might be larger than `n`. This simplifies the logic and avoids unnecessary branching.
+
+3. **Avoiding Negative Values**: Swapping prevents negative values from being passed to the recursive calls. If `n` and `m` are swapped and `m` becomes negative, the recursive multiplication would end naturally without going into a potentially infinite recursion.
+
+In summary, the swapping technique in this particular recursive multiplication implementation is used for clarity and simplicity in handling the recursion, ensuring that the larger value is consistently treated as `n` to simplify the logic and avoid issues with negative values. It's not about efficiency, but rather about maintaining a clear and reliable recursive process.
