2052e16b8bfbfe7006b0db61c7676a9733289e66
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-08-20 19:32:23 +0530

    ll

---
+++ LINKED LIST/FOR NOTES/insertAtFirst.java
@@ -0,0 +1,165 @@
+
+//this program is basic of linked list.
+//pepcoding.
+//use this as notes before interview for understand the linked list.
+
+public class Main {
+  public static class Node {
+    int data;
+    Node next;
+  }
+
+  public static class LinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    void addLast(int val) {
+      Node temp = new Node();
+      temp.data = val;
+      temp.next = null;
+
+      if (size == 0) {
+        head = tail = temp;
+      } else {
+        tail.next = temp;
+        tail = temp;
+      }
+
+      size++;
+    }
+
+    public int size() {
+      return size;
+    }
+
+    public void display() {
+      for (Node temp = head; temp != null; temp = temp.next) {
+        System.out.print(temp.data + " ");
+      }
+      System.out.println();
+    }
+
+    public void removeFirst() {
+      if (size == 0) {
+        System.out.println("List is empty");
+      } else if (size == 1) {
+        head = tail = null;
+        size = 0;
+      } else {
+        head = head.next;
+        size--;
+      }
+    }
+
+    public int getFirst() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return head.data;
+      }
+    }
+
+    public int getLast() {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else {
+        return tail.data;
+      }
+    }
+
+    public int getAt(int idx) {
+      if (size == 0) {
+        System.out.println("List is empty");
+        return -1;
+      } else if (idx < 0 || idx >= size) {
+        System.out.println("Invalid arguments");
+        return -1;
+      } else {
+        Node temp = head;
+        for (int i = 0; i < idx; i++) {
+          temp = temp.next;
+        }
+        return temp.data;
+      }
+    }
+
+
+
+
+//this is actual code to learn.
+
+
+
+    public void addFirst(int val) {
+    
+    //makeing new node.
+    Node n=new Node();
+    
+    //adding the data to the new node.
+    n.data=val;
+    
+    //now new node become ths first node.
+    //this also handle the special case.
+    n.next=head;
+    
+      // now our new node become head
+      head=n;
+    
+    //if size ==0 that means it is first node to add.
+    //in this case tail also point to the first node.
+    if(size==0)
+    {
+      tail=n;
+    }
+    
+    //and increase the size .
+    size++;
+    
+    
+      
+      
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    LinkedList list = new LinkedList();
+
+    String str = br.readLine();
+    while (str.equals("quit") == false) {
+      if (str.startsWith("addLast")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addLast(val);
+      } else if (str.startsWith("size")) {
+        System.out.println(list.size());
+      } else if (str.startsWith("display")) {
+        list.display();
+      } else if (str.startsWith("removeFirst")) {
+        list.removeFirst();
+      } else if (str.startsWith("getFirst")) {
+        int val = list.getFirst();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("getLast")) {
+        int val = list.getLast();
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("getAt")) {
+        int idx = Integer.parseInt(str.split(" ")[1]);
+        int val = list.getAt(idx);
+        if (val != -1) {
+          System.out.println(val);
+        }
+      } else if (str.startsWith("addFirst")) {
+        int val = Integer.parseInt(str.split(" ")[1]);
+        list.addFirst(val);
+      }
+      str = br.readLine();
+    }
+  }
+}
