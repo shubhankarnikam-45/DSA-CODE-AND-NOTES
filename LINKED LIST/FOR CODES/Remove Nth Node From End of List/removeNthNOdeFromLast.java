f83c1da5d6d41ef82977eb8ba63a56597000d495
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-09-04 11:50:31 +0530

    remove node from last

---
+++ LINKED LIST/FOR CODES/Remove Nth Node From End of List/removeNthNOdeFromLast.java
@@ -0,0 +1,33 @@
+
+//this quetion encounter when i give the dsa 2 moak.
+//i solve
+//but facing problem in one testcase.
+// [1,2] n==2
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < n; i++) fast = fast.next;
+       
+        if (fast == null) return head.next;
+        
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
