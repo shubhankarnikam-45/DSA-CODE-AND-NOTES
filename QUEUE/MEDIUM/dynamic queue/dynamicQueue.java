303655424e696f3becebd9bdce625a4a7794d0df
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-09-14 11:08:35 +0530

    fc

---
+++ QUEUE/MEDIUM/dynamic queue/dynamicQueue.java
@@ -0,0 +1,156 @@
+//focus on add function 'overflow' condtion code.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static class CustomQueue {
+    int[] data;
+    int front;
+    int size;
+
+    public CustomQueue(int cap) {
+      data = new int[cap];
+      front = 0;
+      size = 0;
+    }
+
+    int size() {
+      // write ur code here
+      return size;
+    }
+
+    void display() {
+      // write ur code here
+      for(int i = 0; i < size; i++){
+        int idx = (front + i) % data.length;
+        System.out.print(data[idx] + " ");
+      }
+      System.out.println();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // change this code
+    void add(int val) {
+      // write ur code here
+      if(size == data.length){
+        
+        //creating new array.
+        int narr[]=new int[2*data.length];
+        
+        //using for loop inserting value in the new array.
+        for(int i=0;i<size;i++)
+        {
+            int idx=(front+i)%data.length;
+            narr[i]=data[idx];
+        }
+        
+        //pointing to the aray.
+        data=narr;
+        
+        //front point to the 0th index of new array.
+        front=0;
+        
+        int idx = (front + size) % data.length;
+        data[idx] = val;
+        size++;
+        
+      } else {
+        int idx = (front + size) % data.length;
+        data[idx] = val;
+        size++;
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    int remove() {
+      // write ur code here
+      if(size == 0){
+        System.out.println("Queue underflow");
+        return -1;
+       } else {
+        int val = data[front];
+
+        front = (front + 1) % data.length;
+        size--;
+
+        return val;
+       }
+    }
+
+    int peek() {
+       // write ur code here
+       if(size == 0){
+        System.out.println("Queue underflow");
+        return -1;
+       } else {
+        int val = data[front];
+        return val;
+       }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    CustomQueue qu = new CustomQueue(n);
+
+    String str = br.readLine();
+    while(str.equals("quit") == false){
+      if(str.startsWith("add")){
+        int val = Integer.parseInt(str.split(" ")[1]);
+        qu.add(val);
+      } else if(str.startsWith("remove")){
+        int val = qu.remove();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("peek")){
+        int val = qu.peek();
+        if(val != -1){
+          System.out.println(val);
+        }
+      } else if(str.startsWith("size")){
+        System.out.println(qu.size());
+      } else if(str.startsWith("display")){
+        qu.display();
+      }
+      str = br.readLine();
+    }
+  }
+}
