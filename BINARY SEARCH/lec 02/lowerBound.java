5444ae491bc60661f8c781f7df579f87c234806b
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2023-10-14 15:33:26 +0530

    lower bound

---
+++ BINARY SEARCH/lec 02/lowerBound.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public static int lowerBound(int []arr, int n, int x) {
+        
+
+        int low=0;
+        int high=n-1;
+
+        //to store the final answer.
+        //if we not found any value then we return size of array.
+        int ans=n;
+
+        while(low<=high)
+        {
+            //finding middle value
+            int mid=(low+high)/2;
+
+            if(arr[mid]>=x)
+            {
+                //probably answer.
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                //going towards right.
+                low=mid+1;
+            }
+
+        }
+
+        //return answer.
+        return ans;
+        
+    }
+}
